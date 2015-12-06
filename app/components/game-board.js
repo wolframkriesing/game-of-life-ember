86e7fe6cc65069f589b68303c5fae3df2d983818
Wolfram Kriesing <wolfram+github@kriesing.de>
2015-12-06 22:48:59 +0100

    Extract the board.

--- app/components/game-board.js
+++ app/components/game-board.js
@@ -1,23 +1,13 @@
 import Ember from 'ember';
-import Cell from 'game-of-life/models/cell';
+import Board from 'game-of-life/models/board';
 import computed from 'ember-computed-decorators';
-
-const wrapAround = function(index, length) {
-  if (index === -1) {
-    return length - 1;
-  } else if (index === length) {
-    return 0;
-  } else {
-    return index;
-  }
-};
 
 export default Ember.Component.extend({
   playing: false,
 
   @computed('width', 'height')
   board() {
-    return this._setupBoard();
+    return new Board()._setupBoard();
   },
 
   actions: {
@@ -44,36 +34,6 @@ export default Ember.Component.extend({
     if (shouldRepeat) {
       this.set('runLater', Ember.run.next(this, this._step, true));
     }
-  },
+  }
 
-  _setupBoard() {
-    const board = [
-      [Cell.create(), Cell.create(), Cell.create(), Cell.create({alive:true}), Cell.create()],
-      [Cell.create(), Cell.create(), Cell.create(), Cell.create(), Cell.create({alive:true})],
-      [Cell.create(), Cell.create(), Cell.create({alive:true}), Cell.create({alive:true}), Cell.create({alive:true})],
-      [Cell.create(), Cell.create(), Cell.create(), Cell.create(), Cell.create()],
-      [Cell.create(), Cell.create(), Cell.create(), Cell.create(), Cell.create()],
-    ];
-    this._setNeighboursForCell(board);
-    return board;
-  },
-
-  _setNeighboursForCell(board) {
-    return board.map(function(row, row_index, board) {
-      return row.map(function(cell, cell_index) {
-        const neighbours = [
-          board[wrapAround(row_index - 1, board.length)][wrapAround(cell_index - 1, row.length)],
-          board[wrapAround(row_index - 1, board.length)][wrapAround(cell_index, row.length)],
-          board[wrapAround(row_index - 1, board.length)][wrapAround(cell_index + 1, row.length)],
-          board[wrapAround(row_index, board.length)][wrapAround(cell_index - 1, row.length)],
-          board[wrapAround(row_index, board.length)][wrapAround(cell_index + 1, row.length)],
-          board[wrapAround(row_index + 1, board.length)][wrapAround(cell_index - 1, row.length)],
-          board[wrapAround(row_index + 1, board.length)][wrapAround(cell_index, row.length)],
-          board[wrapAround(row_index + 1, board.length)][wrapAround(cell_index + 1, row.length)]
-        ];
-        cell.neighbours = neighbours;
-        return cell;
-      });
-    });
-  },
 });
---
+++ app/models/board.js
@@ -0,0 +1,47 @@
+import Cell from 'game-of-life/models/cell';
+
+const wrapAround = function(index, length) {
+  if (index === -1) {
+    return length - 1;
+  } else if (index === length) {
+    return 0;
+  } else {
+    return index;
+  }
+};
+
+export default class Board {
+
+  _setupBoard() {
+    const board = [
+      [Cell.create(), Cell.create(), Cell.create(), Cell.create({alive:true}), Cell.create()],
+      [Cell.create(), Cell.create(), Cell.create(), Cell.create(), Cell.create({alive:true})],
+      [Cell.create(), Cell.create(), Cell.create({alive:true}), Cell.create({alive:true}), Cell.create({alive:true})],
+      [Cell.create(), Cell.create(), Cell.create(), Cell.create(), Cell.create()],
+      [Cell.create(), Cell.create(), Cell.create(), Cell.create(), Cell.create()]
+    ];
+    this._setNeighboursForCell(board);
+    return board;
+  }
+
+  _setNeighboursForCell(board) {
+    return board.map(function(row, row_index, board) {
+      return row.map(function(cell, cell_index) {
+        const neighbours = [
+          board[wrapAround(row_index - 1, board.length)][wrapAround(cell_index - 1, row.length)],
+          board[wrapAround(row_index - 1, board.length)][wrapAround(cell_index, row.length)],
+          board[wrapAround(row_index - 1, board.length)][wrapAround(cell_index + 1, row.length)],
+          board[wrapAround(row_index, board.length)][wrapAround(cell_index - 1, row.length)],
+          board[wrapAround(row_index, board.length)][wrapAround(cell_index + 1, row.length)],
+          board[wrapAround(row_index + 1, board.length)][wrapAround(cell_index - 1, row.length)],
+          board[wrapAround(row_index + 1, board.length)][wrapAround(cell_index, row.length)],
+          board[wrapAround(row_index + 1, board.length)][wrapAround(cell_index + 1, row.length)]
+        ];
+        cell.neighbours = neighbours;
+        return cell;
+      });
+    });
+  }
+
+
+}
